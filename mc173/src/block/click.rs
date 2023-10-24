ba71e08554a79f4c0b0c3759d5c6196ad022caff
theor <contact@theorozier.fr>
2023-10-24 19:01:15 +0200

    Added lever interaction.

--- mc173/src/block/click.rs
+++ mc173/src/block/click.rs
@@ -11,6 +11,7 @@ use crate::block;
 pub fn click_at(world: &mut World, pos: IVec3, id: u8, metadata: u8) -> bool {
     match id {
         block::BUTTON => click_button(world, pos, metadata),
+        block::LEVER => click_lever(world, pos, metadata),
         _ => return false
     }
 }
@@ -32,4 +33,11 @@ fn click_button(world: &mut World, pos: IVec3, mut metadata: u8) -> bool {
     
     true
 
+}
+
+fn click_lever(world: &mut World, pos: IVec3, mut metadata: u8) -> bool {
+    let active = block::lever::is_active(metadata);
+    block::lever::set_active(&mut metadata, !active);
+    world.set_block_and_metadata(pos, block::LEVER, metadata);
+    true
 }
