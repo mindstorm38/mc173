58cace94f43cf461fc029baeeb75cddf0964384f
Theo Rozier <contact@theorozier.fr>
2023-12-02 16:43:32 +0100

    Added an example for sizeof

---
+++ mc173/examples/sizeof.rs
@@ -0,0 +1,11 @@
+//! This example is just used internally to debug structures sizes.
+
+use std::mem::size_of;
+
+pub fn main() {
+
+    println!("mc173::chunk::Chunk: {}", size_of::<mc173::chunk::Chunk>());
+    println!("mc173::world::World: {}", size_of::<mc173::world::World>());
+    println!("mc173::entity::Entity: {}", size_of::<mc173::entity::Entity>());
+
+}
